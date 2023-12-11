5f507de933209c2ae4a3df88aaa12a69bf0e93b4
Jesper Kjeldgaard <thejspr@gmail.com>
2023-12-11 20:20:00 +0100

    img and title

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@ import TaxCalculator from './components/TaxCalculator.vue'
 
 <template>
   <main class="">
-    <div class="container mx-auto max-w-2xl">
+    <div class="container mx-auto max-w-2xl mt-[5%]">
+      <h1 class="text-white text-4xl font-bold text-center">Skatman</h1>
       <TaxCalculator />
     </div>
   </main>
