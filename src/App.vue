0771e33bc109d41689d4cf6645624c6c2b74d80a
Jesper Kjeldgaard <thejspr@gmail.com>
2023-12-12 09:35:43 +0100

    add footer

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,15 @@ import TaxCalculator from './components/TaxCalculator.vue'
     <div class="container mx-auto max-w-2xl mt-[5%]">
       <h1 class="text-white text-4xl font-bold text-center">Skatman</h1>
       <TaxCalculator />
+      <div class="text-center text-white text-md">
+        &middot;
+        Lavet af <a href="https://webyard.dk" target="_blank" class="text-blue-500">webyard</a>
+        &middot;
+        {{ new Date().getFullYear() }}
+        &middot;
+        <a href="mailto:j@webyard.io" target="_blank" class="text-blue-500">kontakt</a>
+        &middot;
+      </div>
     </div>
   </main>
 </template>
